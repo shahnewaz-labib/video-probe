69ade0346dc312df97e62686f7ad8e70327a6992
renzhamin <renzhamin@gmail.com>
2024-02-17 20:31:39 +0600

    added clerk

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,11 +1,15 @@
 import { ThemeToggle } from "@/components/theme-toggle"
+import { UserButton, auth } from "@clerk/nextjs"
 
-export const NavBar = () => {
+export const NavBar = async () => {
+    const { userId } = auth()
+
     return (
         <div className="bg-accent flex h-16 w-full items-center gap-4 px-32">
             <p>IUT GENESIS</p>
             <div className="flex-center ml-auto gap-4">
                 <ThemeToggle />
+                {userId && <UserButton />}
             </div>
         </div>
     )
